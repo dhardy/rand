24a1c605e36798fb53c37a5206ac1a65ce279424
Diggory Hardy <git@dhardy.name>
2017-11-17 10:56:58 +0000

    update doc

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
 implementors["libc"] = [];
-implementors["rand"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"rand/struct.Error.html\" title=\"struct rand::Error\">Error</a>",];
+implementors["rand"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"rand/struct.Error.html\" title=\"struct rand::Error\">Error</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"rand/jitter_rng/enum.TimerError.html\" title=\"enum rand::jitter_rng::TimerError\">TimerError</a>",];
 implementors["rand_core"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">stdError</a> for <a class=\"struct\" href=\"rand_core/struct.Error.html\" title=\"struct rand_core::Error\">Error</a>",];
 
             if (window.register_implementors) {
