b1215f56ca29dd151656f5b66f20db88f82930c6
Diggory Hardy <git@dhardy.name>
2018-01-06 13:05:16 +0000

    Update doc

--- implementors/rand/trait.CryptoRng.js
+++ implementors/rand/trait.CryptoRng.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["rand"] = ["impl <a class=\"trait\" href=\"rand/trait.CryptoRng.html\" title=\"trait rand::CryptoRng\">CryptoRng</a> for <a class=\"struct\" href=\"rand/jitter_rng/struct.JitterRng.html\" title=\"struct rand::jitter_rng::JitterRng\">JitterRng</a>","impl <a class=\"trait\" href=\"rand/trait.CryptoRng.html\" title=\"trait rand::CryptoRng\">CryptoRng</a> for <a class=\"struct\" href=\"rand/prng/struct.ChaChaRng.html\" title=\"struct rand::prng::ChaChaRng\">ChaChaRng</a>",];
+implementors["rand"] = ["impl <a class=\"trait\" href=\"rand/trait.CryptoRng.html\" title=\"trait rand::CryptoRng\">CryptoRng</a> for <a class=\"struct\" href=\"rand/jitter/struct.JitterRng.html\" title=\"struct rand::jitter::JitterRng\">JitterRng</a>","impl <a class=\"trait\" href=\"rand/trait.CryptoRng.html\" title=\"trait rand::CryptoRng\">CryptoRng</a> for <a class=\"struct\" href=\"rand/prng/struct.ChaChaRng.html\" title=\"struct rand::prng::ChaChaRng\">ChaChaRng</a>","impl <a class=\"trait\" href=\"rand/trait.CryptoRng.html\" title=\"trait rand::CryptoRng\">CryptoRng</a> for <a class=\"struct\" href=\"rand/prng/struct.Hc128Rng.html\" title=\"struct rand::prng::Hc128Rng\">Hc128Rng</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
